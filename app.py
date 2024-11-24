5d6396f483c4a0af2f73a9e1e7fbb414004e1b88
EricSanchez <erc.sanchez@gmail.com>
2024-11-24 16:15:28 -0300

    Parte04

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import time
 
 def fetch_page():
@@ -14,17 +15,29 @@ def parser_page(html):
     old_price : int = int(prices[0].get_text().replace('.',''))
     new_price : int = int(prices[1].get_text().replace('.',''))
        
+    timestamp = time.strftime('%Y-%m-%d %h:%M:%S')   
+    
+    
     return {
         'product_name' : product_name,
         'old_price' : old_price,
         'new_price' : new_price
     }
     
+def save_to_dataframe(produto_info, df) :
+
+    linha = pd.DataFrame ([produto_info])
+    df = pd.concat ([linha, df], ignore_index = True)
+    return df
     
     
 if __name__ == "__main__":
+    
+    df = pd.DataFrame()
+    
     while True:
         page_content = fetch_page()
         produto_info = parser_page(page_content)
-        print (produto_info)
+        df = save_to_dataframe(produto_info, df)
+        print (df)
         time.sleep(10)
